7d2143ff3b7951b528027b06a5ec5fde4701a00e
umair-shehzad <umairshehzad2244@gmail.com>
2022-02-15 19:12:07 +0500

    Changes in server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(process.env.PORT || 5000, function () {
   console.log(
-    "Express server listening on port %d in %s mode",
+    "Server is listening on port %d in %s mode",
     this.address().port,
     app.settings.env
   );
